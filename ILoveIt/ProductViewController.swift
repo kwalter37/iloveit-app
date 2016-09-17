937b2b356b43bfdbab0d1cf49b153d6361236b00
Kevin Walter <kwalter@Kevins-MacBook-Pro.local>
2016-09-16 15:55:09 -1000

    Added type-ahead for category (hardcode rather than DB for now)

--- ILoveIt/ProductViewController.swift
+++ ILoveIt/ProductViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ProductViewController: UIViewController, UITextFieldDelegate {
+class ProductViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
     
     // MARK: Properties
     @IBOutlet weak var searchTextField: UITextField!
@@ -16,8 +16,13 @@ class ProductViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var catTextField: UITextField!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var autocompleteTableView: UITableView!
     
     var product: Product?
+    
+    // for auto-complete of category
+    var existingCategories = ["Pasta", "Pasta Sauce", "Crackers"]
+    var autocompleteCategories = [String]()
     
     // MARK: Navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -65,6 +70,24 @@ class ProductViewController: UIViewController, UITextFieldDelegate {
     func textFieldDidEndEditing(textField: UITextField) {
         checkValidProductValues()
         navigationItem.title = textField.text
+        autocompleteTableView.hidden = true
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool
+    {
+        
+        if textField == catTextField {
+            // move the autopopulate list view to just under category and same width
+            let catTextFrame = self.view.convertRect(catTextField.frame, fromView:catTextField.superview)
+            autocompleteTableView.frame.origin.y = catTextFrame.origin.y + catTextFrame.height
+            autocompleteTableView.frame.origin.x = catTextFrame.origin.x
+            autocompleteTableView.frame.size.width = catTextFrame.size.width
+            
+            // see if we have a match
+            let substring = (catTextField.text! as NSString).stringByReplacingCharactersInRange(range, withString: string)
+            searchAutocompleteEntriesWithSubstring(substring)
+        }
+        return true     // not sure about this - could be false
     }
     
     func checkValidProductValues() {
@@ -95,11 +118,61 @@ class ProductViewController: UIViewController, UITextFieldDelegate {
         
         // Enable the Save button only if the text field has a valid Meal name.
         checkValidProductValues()
+        
+        // auto-complete
+        autocompleteTableView.delegate = self
+        autocompleteTableView.dataSource = self
+        autocompleteTableView.scrollEnabled = true
+        autocompleteTableView.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Auto-complete tableview
+    
+    // search for match for type-ahaed (right now onlys supports the one field - cat
+    func searchAutocompleteEntriesWithSubstring(substring: String)
+    {
+        autocompleteCategories.removeAll(keepCapacity: false)
+        
+        for curString in existingCategories
+        {
+            let myString:NSString! = curString.lowercaseString as NSString
+            
+            let substringRange : NSRange! = myString.rangeOfString(substring.lowercaseString)
+            
+            if (substringRange.location == 0)
+            {
+                autocompleteCategories.append(curString)
+            }
+        }
+        
+        // show the autocomplete view if we have a match
+        autocompleteTableView.hidden = autocompleteCategories.count < 1
+        autocompleteTableView.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return autocompleteCategories.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let autoCompleteRowIdentifier = "AutoCompleteRowIdentifier"
+        let cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier(autoCompleteRowIdentifier, forIndexPath: indexPath) as UITableViewCell
+        let index = indexPath.row as Int
+        
+        cell.textLabel!.text = autocompleteCategories[index]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let selectedCell : UITableViewCell = tableView.cellForRowAtIndexPath(indexPath)!
+        catTextField.text = selectedCell.textLabel!.text
+        autocompleteTableView.hidden = true
     }
 
 }
